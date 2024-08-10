201dc78a87b092cb0765bb0d79df856e572070bc
Naeem Raza <Naeem@hazentech.com>
2024-08-10 23:21:06 +0500

    Add project files.

---
+++ WDApiClient/CsvGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace WDApiClient;
+
+internal class CsvGenerator
+{
+	internal static void CreateHeader<T>(List<T> list, StreamWriter sw)
+	{
+		PropertyInfo[] properties = typeof(T).GetProperties();
+		for (int i = 0; i < properties.Length - 1; i++)
+		{
+			sw.Write(properties[i].Name + ",");
+		}
+		var lastProp = properties[properties.Length - 1].Name;
+		sw.Write(lastProp + sw.NewLine);
+	}
+
+	internal static void CreateRows<T>(List<T> list, StreamWriter sw)
+	{
+		foreach (var item in list)
+		{
+			PropertyInfo[] properties = typeof(T).GetProperties();
+			for (int i = 0; i < properties.Length - 1; i++)
+			{
+				var prop = properties[i];
+				sw.Write(prop.GetValue(item) + ",");
+			}
+			var lastProp = properties[properties.Length - 1];
+			sw.Write(lastProp.GetValue(item) + sw.NewLine);
+		}
+	}
+}
---
+++ WDApiClient/Program.cs
@@ -0,0 +1,3 @@
+﻿using WDApiClient;
+
+await VehicleProcessor.ProcessVehicles();
